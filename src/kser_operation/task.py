00a2f088c4b246ed709b69ecfb85d118130af540
Cédric Dumay <cedric.dumay@gmail.com>
2017-12-19 12:42:05 +0100

    feat: log method in tasks for running outputs

--- src/kser_operation/task.py
+++ src/kser_operation/task.py
@@ -6,7 +6,10 @@
 
 
 """
+import logging
 from kser.entry import Entrypoint
+
+logger = logging.getLogger(__name__)
 
 
 class Task(Entrypoint):
@@ -25,6 +28,13 @@ class Task(Entrypoint):
     def get_attr(self, item):
         attr = "{}Id".format(item)
         return attr, getattr(getattr(self, item, self), attr, None)
+
+    def log(self, message, level=logging.INFO, *args, **kwargs):
+        msg = "{}.{}: {}[{}]: {}".format(
+            self.__class__.__name__, self.status, self.__class__.path,
+            self.uuid, message
+        )
+        return logger.log(level=level, msg=msg, *args, **kwargs)
 
     def __repr__(self):
         return "Task [{}](uuid={},status={}, params={})".format(
